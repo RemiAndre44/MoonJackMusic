3d7933e64e267123aa0fce02b8e5919e153c0a34
RemiAndre44 <remi44andre@gmail.com>
2018-05-13 20:07:08 +0200

    Implémentation de room dans gradle

--- app/src/main/java/com/example/santikush/moonjackmusicapplication/database/MoonJackDB.java
+++ app/src/main/java/com/example/santikush/moonjackmusicapplication/database/MoonJackDB.java
@@ -1,4 +1,16 @@
 package com.example.santikush.moonjackmusicapplication.database;
 
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
 
+import com.example.santikush.moonjackmusicapplication.database.dao.Track;
+
+@Database(entities = {Track.class}, version = 4)
+public abstract class MoonJackDB extends RoomDatabase {
+
+    public static final String DATABASE_NAME = "MoonJack";
+
+
+}
 
